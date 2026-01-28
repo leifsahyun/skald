9810e08f8e05d13e41cca46a34ee162327361c37
leifsahyun <33590091+leifsahyun@users.noreply.github.com>
2026-01-27 22:53:37 -0500

    Optimize coastline graphics creation
    Replace individual path drawing with svg method.

--- js/sailing-game.js
+++ js/sailing-game.js
@@ -470,15 +470,8 @@ class SailingGame {
             // Create graphics if not already created
             if (!this.coastline.graphics.has(key)) {
                 const g = new PIXI.Graphics();
-                
-                const paths = chunk.svgData.querySelectorAll('path');
-                paths.forEach(pathElement => {
-                    const pathData = pathElement.getAttribute('d');
-                    if (pathData) {
-                        this.drawSVGPath(g, pathData, scale);
-                    }
-                });
-                
+
+                g.svg(pathData);
                 g.stroke({ width: 1.0 / scale, color: 0x654321 });
                 
                 this.coastlineContainer.addChild(g);
